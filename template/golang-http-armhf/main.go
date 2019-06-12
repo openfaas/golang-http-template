a1cf458a88c8f7ecdaeb5310937b36008865c813
Bruno Miguel Custódio <brunomcustodio@gmail.com>
2019-06-12 16:46:01 +0100

    Allow for customizing the read and write timeouts
    Signed-off-by: Bruno Miguel Custódio <brunomcustodio@gmail.com>

--- template/golang-http-armhf/main.go
+++ template/golang-http-armhf/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"handler/function"
@@ -60,11 +61,19 @@ func makeRequestHandler() func(http.ResponseWriter, *http.Request) {
 func main() {
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", 8082),
-		ReadTimeout:    3 * time.Second,
-		WriteTimeout:   3 * time.Second,
+		ReadTimeout:    mustParseDuration(os.Getenv("read_timeout")),
+		WriteTimeout:   mustParseDuration(os.Getenv("write_timeout")),
 		MaxHeaderBytes: 1 << 20, // Max header of 1MB
 	}
 
 	http.HandleFunc("/", makeRequestHandler())
 	log.Fatal(s.ListenAndServe())
+}
+
+func mustParseDuration(v string) time.Duration {
+	d, err := time.ParseDuration(v)
+	if err != nil {
+		panic(err)
+	}
+	return d
 }
--- template/golang-middleware/main.go
+++ template/golang-middleware/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"handler/function"
@@ -13,11 +14,20 @@ import (
 func main() {
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", 8082),
-		ReadTimeout:    3 * time.Second,
-		WriteTimeout:   3 * time.Second,
+		Addr:           fmt.Sprintf(":%d", 8082),
+		ReadTimeout:    mustParseDuration(os.Getenv("read_timeout")),
+		WriteTimeout:   mustParseDuration(os.Getenv("write_timeout")),
 		MaxHeaderBytes: 1 << 20, // Max header of 1MB
 	}
 
 	http.HandleFunc("/", function.Handle)
 	log.Fatal(s.ListenAndServe())
+}
+
+func mustParseDuration(v string) time.Duration {
+	d, err := time.ParseDuration(v)
+	if err != nil {
+		panic(err)
+	}
+	return d
 }
