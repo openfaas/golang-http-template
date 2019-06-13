2a8f0be83d0da3a56d182110a8653db929460af5
Alex Ellis <alexellis2@gmail.com>
2019-06-13 17:48:51 +0100

    Read timeout values from env-vars
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- template/golang-middleware-armhf/main.go
+++ template/golang-middleware-armhf/main.go
@@ -4,17 +4,37 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"handler/function"
 	//"github.com/openfaas-incubator/golang-http-template/template/golang-middleware/function"
 )
 
+func parseIntOrDurationValue(val string, fallback time.Duration) time.Duration {
+	if len(val) > 0 {
+		parsedVal, parseErr := strconv.Atoi(val)
+		if parseErr == nil && parsedVal >= 0 {
+			return time.Duration(parsedVal) * time.Second
+		}
+	}
+
+	duration, durationErr := time.ParseDuration(val)
+	if durationErr != nil {
+		return fallback
+	}
+	return duration
+}
+
 func main() {
+	readTimeout := parseIntOrDurationValue(os.Getenv("read_timeout"), 10*time.Second)
+	writeTimeout := parseIntOrDurationValue(os.Getenv("write_timeout"), 10*time.Second)
+
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", 8082),
-		ReadTimeout:    3 * time.Second,
-		WriteTimeout:   3 * time.Second,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
 		MaxHeaderBytes: 1 << 20, // Max header of 1MB
 	}
 
