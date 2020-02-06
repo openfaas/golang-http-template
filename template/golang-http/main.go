e6ceaf41a828a8bc62918e3f8a3e40cbb722dea4
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-02-06 10:49:55 +0000

    Adds graceful shutdown for golang -middleware and -http
    Closes: #35
    
    Tested outside of a container, using kill -s TERM <PID> - it
    worked as expected by waiting for N seconds.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- template/golang-http/main.go
+++ template/golang-http/main.go
@@ -1,18 +1,79 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"sync/atomic"
+	"syscall"
 	"time"
 
 	"handler/function"
-	// "github.com/alexellis/golang-http-template/template/golang-http/function"
+
 	handler "github.com/openfaas-incubator/go-function-sdk"
 )
+
+var (
+	acceptingConnections int32
+)
+
+const defaultTimeout = 10 * time.Second
+
+func main() {
+	readTimeout := parseIntOrDurationValue(os.Getenv("read_timeout"), defaultTimeout)
+	writeTimeout := parseIntOrDurationValue(os.Getenv("write_timeout"), defaultTimeout)
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", 8082),
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: 1 << 20, // Max header of 1MB
+	}
+
+	http.HandleFunc("/", makeRequestHandler())
+	listenUntilShutdown(s, writeTimeout)
+}
+
+func listenUntilShutdown(s *http.Server, shutdownTimeout time.Duration) {
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGTERM)
+
+		<-sig
+
+		log.Printf("[entrypoint] SIGTERM received.. shutting down server in %s\n", shutdownTimeout.String())
+
+		<-time.Tick(shutdownTimeout)
+
+		if err := s.Shutdown(context.Background()); err != nil {
+			log.Printf("[entrypoint] Error in Shutdown: %v", err)
+		}
+
+		log.Printf("[entrypoint] No new connections allowed. Exiting in: %s\n", shutdownTimeout.String())
+
+		<-time.Tick(shutdownTimeout)
+
+		close(idleConnsClosed)
+	}()
+
+	// Run the HTTP server in a separate go-routine.
+	go func() {
+		if err := s.ListenAndServe(); err != http.ErrServerClosed {
+			log.Printf("[entrypoint] Error ListenAndServe: %v", err)
+			close(idleConnsClosed)
+		}
+	}()
+
+	atomic.StoreInt32(&acceptingConnections, 1)
+
+	<-idleConnsClosed
+}
 
 func makeRequestHandler() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -73,19 +134,4 @@ func parseIntOrDurationValue(val string, fallback time.Duration) time.Duration {
 		return fallback
 	}
 	return duration
-}
-
-func main() {
-	readTimeout := parseIntOrDurationValue(os.Getenv("read_timeout"), 10*time.Second)
-	writeTimeout := parseIntOrDurationValue(os.Getenv("write_timeout"), 10*time.Second)
-
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", 8082),
-		ReadTimeout:    readTimeout,
-		WriteTimeout:   writeTimeout,
-		MaxHeaderBytes: 1 << 20, // Max header of 1MB
-	}
-
-	http.HandleFunc("/", makeRequestHandler())
-	log.Fatal(s.ListenAndServe())
 }
