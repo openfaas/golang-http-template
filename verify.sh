03829a6dee4be0aaea92f4df78e736981fd59366
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2025-04-22 17:49:32 +0100

    Fix CI job
    The name of YAML files is now stack.yaml as a default
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- verify.sh
+++ verify.sh
@@ -9,7 +9,7 @@ build_template() {
     echo Building $template
     func_name=$template-ci
     $CLI new $func_name --lang $template 2>/dev/null 1>&2
-    $CLI build -f $func_name.yml
+    $CLI build
 }
 
 verify_and_clean() {
@@ -74,4 +74,4 @@ done
 
 # remove the generated files and folders if successful
 cd ../
-rm -rf *-ci *-ci.yml
+rm -rf *-ci stack.yaml
